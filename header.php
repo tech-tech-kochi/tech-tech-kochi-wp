d5d2d35d7cee723ef2fbaba57216335876e4ed2b
michiyo <jam3748@gmail.com>
2021-11-28 23:37:56 +0900

    header.phpを修正

--- header.php
+++ header.php
@@ -7,10 +7,39 @@
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
+<style>
+    li {
+        display: inline;
+        margin-right: 20px;
+    }
+    a {
+        position: relative;
+    }
+    a::after {
+        position: absolute;
+        bottom: -4px;
+        left: 0;
+        content: '';
+        width: 100%;
+        height: 1px;
+        background: #333;
+        transform: scale(0, 1);
+        transform-origin: left top;
+        transition: transform .3s;
+    }
+    a:hover::after {
+        transform: scale(1, 1);
+    }
+    .header {
+        padding: 20px;
+        justify-content: space-between;
+    }
+</style>
 
 <body <?php body_class(); ?>>
-<header class="header">
-これはヘッダーです
+<header class="header bg-yel flex">
+    <a href="<?php echo home_url( '/' ); ?>">TECH-TECH KOCHI</a>
+
 
     <!-- ナビメニューを表示 -->
     <?php wp_nav_menu(
