8d9e0165587d39ca2425e0e80b8477a2092303a4
nakaoka moe <moesuke2015@gmail.com>
2021-12-09 20:27:34 +0900

    エラー画面とフッターを修正

--- 404.php
+++ 404.php
@@ -2,9 +2,17 @@
 
 
 
-これはエラーページです。
+<p>さーせん。このページは存在しないみたいですね。（あ、エラー画面は存在しますよ！）
+</p>
 
 
 
 <?php get_footer(); ?>
+<style>
+    p {
+        color:red;
+        line-height:300px;
+    }
+     
+</style>
 
--- footer.php
+++ footer.php
@@ -1,5 +1,57 @@
 
-<footer>これはフッターです</footer>
+<footer class="bg-gry"></footer>
 
 </body>
+
+<style>
+     li {
+        display: inline;
+        margin-right: 20px;
+    }
+    a {
+        position: relative;
+    }
+    a::after {
+        position: absolute;
+        bottom: -4px;
+        left: 0;
+        content: '';
+        width: 100%;
+        height: 1px;
+        background: #333;
+        transform: scale(0, 1);
+        transform-origin: left top;
+        transition: transform .3s;
+    }
+    a:hover::after {
+        transform: scale(1, 1);
+    }
+   .footer {
+       padding: 10px;
+   }
+   /* .footer a{
+       padding:10px;
+   }
+   .footer li{
+       padding:10px;
+   } */
+
+</style>
+
+<footer class="footer bg-gry flex">
+<div class="container flex">
+
+    <a href="<?php echo home_url( '/' ); ?>">TECH-TECH KOCHI</a>
+
+
+    <!-- ナビメニューを表示 -->
+    <?php wp_nav_menu(
+        array(
+            'menu'  => 'pc_menu',
+            'menu_class' => 'nav navbar-nav',
+        )
+    ); ?>
+
+</div>
+</footer>
 </html>
