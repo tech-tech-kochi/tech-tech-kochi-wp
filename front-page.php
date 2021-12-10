5d80a732253e26d985ab93cad234c1f076049b96
杉内壮也(2020最新) <sugiuchisoya@Sugiuchi-MacBook-Air.local>
2021-12-10 19:07:36 +0900

    問い合わせフォーム作成、トップいじった

--- front-page.php
+++ front-page.php
@@ -7,39 +7,63 @@
             <h1>TECH-TECH KOCHI</h1>
         </div> 
 
-        <div class="main-news">
+        <div class="main-news main-contents">
             <h2 class="contents-title fc-wht ">NEWS</h2>
+            <!-- 投稿記事を表示 -->
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="news-wrapper flex">
+                <div class="date"><?php the_time('Y年m月d日'); ?></div>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </div>
+            <?php endwhile; endif; ?>
 
             <div class="read-more">
-                <a href="">>> Read more</a>
+                <a href="<?php bloginfo('url'); ?>/news/">>> Read more</a>
             </div>
 
         </div>
 
-        <div class="main-about">
+        <div class="main-about main-contents">
             <h2 class="contents-title fc-wht ">ABOUT</h2>
+            <p>サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</p>
 
             <div class="read-more">
-                <a href="">>> Read more</a>
+                <a href="<?php bloginfo('url'); ?>/about/">>> Read more</a>
             </div>
 
         </div>
 
-        <div class="main-activity">
+        <div class="main-activity main-contents">
             <h2 class="contents-title fc-wht ">ACTIVITY</h2>
+            <p>サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</p>
 
             <div class="read-more">
-                <a href="">>> Read more</a>
+                <a href="<?php bloginfo('url'); ?>/activity/">>> Read more</a>
             </div>
             
         </div>
 
-        <div class="main-menber">
+        <div class="main-menber main-contents">
             <h2 class="contents-title fc-wht">MEMBER</h2>
+            <p>サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</p>
 
             <div class="read-more">
-                <a href="">>> Read more</a>
+                <a href="<?php bloginfo('url'); ?>/member/">>> Read more</a>
             </div>
+        </div>
+
+        <div class="main-contact">
+            <h2>CONTACT</h2>
+
         </div>
 
 
--- page-contact.php
+++ page-contact.php
@@ -3,25 +3,48 @@
 Template Name: お問い合わせ
 */
 ?>
-<!DOCTYPE html>
-<html lang="jp">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>活動内容について</title>
-    <?php wp_head(); ?>
-</head>
-<style>
-</style>
-<body>
-    <?php get_header(); ?>
+
+<?php get_header(); ?>
+<main class="main">
     <div class="page-title-container">
-        <p class="page-title fc-wht fs-32px">お問い合わせ</p>
+        <h2 class="page-title fc-wht fs-32px">お問い合わせ</h2>
+    </div>
+    <div class="container">
+        <div class="form">
+            <div class="form-item flex">
+                <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>氏名</p>
+                <input type="text" class="form-item-input" placeholder="例）テクテク太郎">
+            </div>
+            <div class="form-item flex">
+                <p class="form-item-label"><span class="form-item-label-any fc-wht">任意</span>会社名</p>
+                <input type="text" class="form-item-input" placeholder="例）株式会社テクテク高知">
+            </div>
+
+            <div class="form-item flex">
+                <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>電話番号</p>
+                <input type="text" class="form-item-input" placeholder="例）000-0000-0000">
+            </div>
+            <div class="form-item flex">
+                <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>メールアドレス</p>
+                <input type="email" class="form-item-input" placeholder="例）example@gmail.com">
+            </div>
+            <div class="form-item flex">
+                <p class="form-item-label isMsg"><span class="form-item-label-required fc-wht">必須</span>お問い合わせ内容</p>
+                <textarea class="form-item-textarea"></textarea>
+            </div>
+            <input type="submit" class="form-btn" value="送信する >>">
+        </div>
+
     </div>
 
-    これはお問い合わせページです。
-    <?php get_footer(); ?>
-    <?php wp_footer() ?>
-</body>
-</html>
+
+</main>
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
