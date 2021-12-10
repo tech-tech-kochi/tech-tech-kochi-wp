baa478473c44f9bab2635b51c43718091e929053
杉内壮也(2020最新) <sugiuchisoya@Sugiuchi-MacBook-Air.local>
2021-12-10 16:05:32 +0900

    フロントページいじった

--- front-page.php
+++ front-page.php
@@ -2,25 +2,49 @@
 
 
 <main class="main bg-yel">
-    <div class="hero-img container">
-        <h1>TECH-TECH KOCHI</h1>
-    </div> 
+    <div class="container">
+        <div class="hero-img">
+            <h1>TECH-TECH KOCHI</h1>
+        </div> 
 
+        <div class="main-news">
+            <h2 class="contents-title fc-wht ">NEWS</h2>
 
-    <div class="main-about">
-        <h2 class="contents-title"></h2>
+            <div class="read-more">
+                <a href="">>> Read more</a>
+            </div>
+
+        </div>
+
+        <div class="main-about">
+            <h2 class="contents-title fc-wht ">ABOUT</h2>
+
+            <div class="read-more">
+                <a href="">>> Read more</a>
+            </div>
+
+        </div>
+
+        <div class="main-activity">
+            <h2 class="contents-title fc-wht ">ACTIVITY</h2>
+
+            <div class="read-more">
+                <a href="">>> Read more</a>
+            </div>
+            
+        </div>
+
+        <div class="main-menber">
+            <h2 class="contents-title fc-wht">MEMBER</h2>
+
+            <div class="read-more">
+                <a href="">>> Read more</a>
+            </div>
+        </div>
+
 
     </div>
 
-    <div class="main-activity">
-        <h2 class="contents-title"></h2>
-        
-    </div>
-
-    <div class="main-menber">
-        <h2 class="contents-title"></h2>
-    </div>
-    
 </main>
 
 
