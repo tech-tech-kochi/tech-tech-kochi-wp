40675a73769ac272b1d3f87340542673e46e343f
杉内壮也(2020最新) <sugiuchisoya@Sugiuchi-MacBook-Air.local>
2021-12-10 19:29:44 +0900

    フォームにドロップダウンの件名追加

--- page-contact.php
+++ page-contact.php
@@ -24,10 +24,24 @@ Template Name: お問い合わせ
                 <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>電話番号</p>
                 <input type="text" class="form-item-input" placeholder="例）000-0000-0000">
             </div>
+
             <div class="form-item flex">
                 <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>メールアドレス</p>
                 <input type="email" class="form-item-input" placeholder="例）example@gmail.com">
             </div>
+
+            <div class="form-item flex">
+                <p class="form-item-label"><span class="form-item-label-required fc-wht">必須</span>件名</p>
+                <select class="form-item-input" name="件名">
+                    <option value="">選択してください</option>
+                    <option value="メンバー募集について">メンバー募集について</option>
+                    <option value="勉強会について">勉強会について</option>
+                    <option value="お仕事のご依頼について">お仕事のご依頼について</option>
+                    <option value="メンバーのスカウト">メンバーのスカウト</option>
+                    <option value="その他">その他</option>
+                </select>    
+            </div>
+
             <div class="form-item flex">
                 <p class="form-item-label isMsg"><span class="form-item-label-required fc-wht">必須</span>お問い合わせ内容</p>
                 <textarea class="form-item-textarea"></textarea>
