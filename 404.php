67457d36c22278773c541586050a6d856f029470
nakaoka moe <moesuke2015@gmail.com>
2021-12-15 20:25:43 +0900

    404修正しました。

--- 404.php
+++ 404.php
@@ -1,18 +1,37 @@
 <?php get_header(); ?>
 
+<body>
+<h1>404</h1>
+<h1>not found</h1>
+<p>さーせん。このページは存在しないみたいですね。</p>
+<p>（あ、エラー画面は存在しますよ！）</p>
+</body>
 
-
-<p>さーせん。このページは存在しないみたいですね。（あ、エラー画面は存在しますよ！）
-</p>
 
 
 
 <?php get_footer(); ?>
+    
 <style>
-    p {
+    h1{
+        font-size:110px;
+        font-weight:bold;
         color:red;
-        line-height:300px;
+        text-align:center;
+        margin:40px;
+        padding:5px;
+    }
+    p {
+        font-size:41px;
+        font-weight:bolder;
+        color:red;
+        text-align:center;
+        margin:50px;
+        padding:15px;
     }
      
+     body{
+        background-color:#fafad2;
+     }
 </style>
 
